729047c58c2a30e3d5300c8dbe65968f0313003b
Srj <sreejithvjn.2005@gmail.com>
2026-07-01 22:32:13 +0530

    Added solution - LeetHub

---
+++ 14556-java-int-to-string-Hackerrank/14556-java-int-to-string.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = String.valueOf(n);
+        
+        if(n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        }
+        else {
+            System.out.println("Wrong answer");
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
